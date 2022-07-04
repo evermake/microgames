afdb73403b35744e534afc2a6514e4b9aaed297f
evermake <vladeryabkin@gmail.com>
2022-07-04 16:43:46 +0300

    feat: create empty vue project

---
+++ web-application/src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    <h1 class="heading">Welcome to microgames!</h1>
+  </main>
+</template>
+
+<style scoped>
+.heading {
+  text-align: center;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 24px;
+}
+</style>
